98b87c57e82e6dd340431c523f9a9f969208cb0c
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-06-22 14:35:35 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Header from '.././components/Header';
 
-test('renders GitHub link', () => {
+test('Renders Header component with GitHub link', () => {
   render(<Header />);
   const linkElement = screen.getByText(/GitHub/i);
   expect(linkElement).toBeInTheDocument();
