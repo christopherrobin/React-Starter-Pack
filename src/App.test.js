2a82e5b44e0e47298b05b786b1f2a0454c3377f8
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-06-22 15:38:56 -0400

    Added landing page component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import Header from '.././components/Header';
+import App from './App';
 
 test('Renders Header component with GitHub link', () => {
-  render(<Header />);
+  render(<App />);
   const linkElement = screen.getByText(/GitHub/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Renders Landing Page component <H1>', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/Hello!/i);
+  expect(headerElement).toBeInTheDocument();
 });
