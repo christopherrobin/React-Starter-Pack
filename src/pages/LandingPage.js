6ac58beb79ae1cfa3e64f3ca921246c365a5c79a
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-07-01 14:26:52 -0400

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -15,20 +15,20 @@ const LandingPage = () => {
 				<Col xs={12}>
 					<h2>Hello!</h2>
 					<p>
-            This is my custom React Starter Pack, a constantly evolving
-            collection of my favorite tools ready to be used to jump-start a new
-            project.
+						This is my custom React Starter Pack, a constantly evolving
+						collection of my favorite tools ready to be used to jump-start a new
+						project.
 					</p>
 					<p>
-            This project initializes with 2 pages and a minimalist
-            react-router-dom navigational example.
+						This project initializes with 2 pages and a minimalist
+						react-router-dom navigational example.
 					</p>
 					<p>Other features include:</p>
 					<ul>
 						<li>Custom fetch and cookie management utility functions</li>
 						<li>ESLint configuration</li>
 						<li>
-              Babel configuration (without ejecting)
+							Babel configuration (without ejecting)
 							<ul>
 								<li>jsx-control-statements</li>
 							</ul>
