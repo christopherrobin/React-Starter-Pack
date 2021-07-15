e301c4c43aa36079a63c0318e811ef2adc4c4cfc
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2021-07-15 16:46:35 -0400

    Custom debounce function without memoization

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,47 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Container, Row, Col } from 'reactstrap';
 import Header from '.././components/Header';
 import './LandingPage.scss';
 
+import faker from 'faker';
+// import { debounce } from 'lodash';
+
+const fakeNames = Array.from(Array(400), () => {
+	return faker.name.findName();
+});
+
+const myDebounce = (operation, wait, immediate) => {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) operation.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) operation.apply(context, args);
+	};
+};
+
 const LandingPage = () => {
+	const [query, setQuery] = useState('');
+
+	let filteredNames = fakeNames;
+
+	if (query !== '') {
+		filteredNames = fakeNames.filter(name => {
+			return name.toLowerCase().includes(query.toLowerCase());
+		});
+	}
+
+	// Create a function that will set the query's state to the new value
+	const changeHandler = event => {
+		setQuery(event.target.value);
+	};
+
+	// Debounce `changeHandler` function so that it only runs after 500ms
+	const debouncedChangeHandler = (() => {
+		return myDebounce(changeHandler, 500);
+	});
+
 	return (
-		<Container id="LandingPage--Container">
-			<Helmet>
-				<title>React-Starter-Pack - LandingPage</title>
-			</Helmet>
-			<Header />
-			<Row>
-				<Col xs={12}>
-					<h2>Hello!</h2>
-					<p>
-						This is my custom React Starter Pack, a constantly evolving
-						collection of my favorite tools ready to be used to jump-start a new
-						project.
-					</p>
-					<p>
-						This project initializes with 2 pages and a minimalist
-						react-router-dom navigational example.
-					</p>
-					<p>Other features include:</p>
-					<ul>
-						<li>Custom fetch and cookie management utility functions</li>
-						<li>ESLint configuration</li>
-						<li>
-							Babel configuration (without ejecting)
-							<ul>
-								<li>jsx-control-statements</li>
-							</ul>
-						</li>
-						<li>Material-UI core, lab, and icons</li>
-						<li>SASS</li>
-						<li>Jest</li>
-						<li>Lodash</li>
-						<li>React-Helmet</li>
-					</ul>
-				</Col>
-			</Row>
-		</Container>
+		<div>
+			<input
+				onChange={debouncedChangeHandler()}
+				type="text"
+				placeholder="Type a query..."
+			/>
+			{filteredNames.map(name => (
+				<div key={name}>{name}</div>
+			))}
+			<div>{filteredNames.length === 0 && query !== '' && 'No matches...'}</div>
+		</div>
 	);
 };
 
